b6b5687dd33d59255d6c626c4dc8b42a1a4a92bd
Tisserandmat <tisserand-mathias_student2020@wilder.school>
2021-10-14 12:12:25 +0200

    WiP

--- Projects/20211014_small_games/src/main/java/wcscda/small_game/Morpion.java
+++ Projects/20211014_small_games/src/main/java/wcscda/small_game/Morpion.java
@@ -2,16 +2,56 @@ package wcscda.small_game;
 
 import java.awt.*;
 import java.awt.image.ImageObserver;
+import java.util.ArrayList;
 
 public class Morpion extends Drawable implements SmallGameInterface {
     @Override
     public void draw(Graphics2D g, ImageObserver io) {
+
         drawGrid(g, io);
 
         g.setFont(new Font("Roboto", Font.BOLD, 50));
-        g.drawString("Morpion", 50, 50);
+
+        drawClick(g, io);
+
+
+
+    }
+    public static void drawClick(Graphics2D g, ImageObserver io ){
+
+        ArrayList position = MouseController.getSavePosXY();
+        for(int i = 0 ;  i < position.size() ; i++){
+            int x = position.indexOf(i);
+            int y = position.indexOf(i+1);
+            MouseController.clickForm( g,  x, y);
+
+
+            i++;
+        }
     }
 
     private void drawGrid(Graphics2D g, ImageObserver io) {
+        int verticalLoops = 2;
+        int horizontaleLoops = 2;
+        int x1 = 0;
+        int x2 = 0;
+        int y1 = 0;
+        int y2 = 0;
+        // utilisation de la methode g.drawLine pour faire les différentes lignes du jeux du morpions
+        // celle-ci comporte 2 boucles une boucle verticale et une autre horizontale et j'incremente les différentes valeurs
+        // a chaque tour de boucle
+
+        for (int i = 0; i < verticalLoops; i++) {
+            g.drawLine(x1 += 400, y1 = y1, x2 += 400, y2 += 800);
+        }
+        x1 = 0;
+        x2 = 0;
+        y1 = 0;
+        y2 = 0;
+
+        for (int j = 0; j < horizontaleLoops; j++) {
+            g.drawLine(x1, y1 += 266, x2 = 1200, y2 += 266);
+
+        }
     }
 }
--- Projects/20211014_small_games/src/main/java/wcscda/small_game/MouseController.java
+++ Projects/20211014_small_games/src/main/java/wcscda/small_game/MouseController.java
@@ -1,13 +1,17 @@
 package wcscda.small_game;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.ImageObserver;
+import java.util.ArrayList;
 
 public class MouseController extends MouseAdapter {
     private final Board board;
+    private  static boolean clik = false;
+    private static ArrayList<Integer> savePosXY = new ArrayList<Integer>();
 
     public MouseController(Board board) {
         this.board = board;
@@ -15,21 +19,44 @@ public class MouseController extends MouseAdapter {
 
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
-        board.redraw(new Drawable(){
+        board.redraw(new Drawable() {
             @Override
             public void draw(Graphics2D g, ImageObserver io) {
                 int x = mouseEvent.getX();
                 int y = mouseEvent.getY();
-
                 g.setColor(Color.WHITE);
-                g.drawLine(x - 30, y - 30, x + 30, y +30);
-                g.drawLine(x + 30, y - 30, x - 30, y +30);
+                clickForm(g, x, y);
+                System.out.println(savePosXY.toString());
             }
-        } );
+        });
     }
 
-    /*@Override
+    public static ArrayList<Integer> getSavePosXY() {
+        return savePosXY;
+    }
+
+    public static void setSavePosXY(ArrayList<Integer> savePosXY) {
+        MouseController.savePosXY = savePosXY;
+    }
+
+    public static void clickForm(Graphics2D g, int x, int y) {
+        if (clik == false) {
+            g.drawLine(x - 30, y - 30, x + 30, y + 30);
+            g.drawLine(x + 30, y - 30, x - 30, y + 30);
+            clik = true;
+            savePosXY.add(x);
+            savePosXY.add(y);
+        } else {
+            g.drawOval(x - 50, y - 50, 100, 100);
+            clik = false;
+            savePosXY.add(x);
+            savePosXY.add(y);
+        }
+    }
+
+
+
     public void mousePressed(MouseEvent mouseEvent) {
 
-    }*/
+    }
 }

6adb08b1ad18625c4e735899bb63e2d2d98bcbf4
HOARAU <Mathieuhoarau@laposte.net>
2021-10-14 18:23:54 +0200

    fin sans verif de clic sur la meme case

--- Projects/20211014_small_games/src/main/java/wcscda/small_game/MouseController.java
+++ Projects/20211014_small_games/src/main/java/wcscda/small_game/MouseController.java
@@ -17,12 +17,10 @@ public class MouseController extends MouseAdapter {
 	private static ArrayList<Integer> posY = new ArrayList<>();
 	private static boolean swipe = true;
 
-	
 	public static boolean isSwipe() {
 		return swipe;
 	}
 
-	
 	public static void setSwipe(boolean swipe) {
 		MouseController.swipe = swipe;
 	}
@@ -56,111 +54,125 @@ public class MouseController extends MouseAdapter {
 					}
 				}
 				if (mouseEvent.getX() < 400 && mouseEvent.getY() < 266) {
-					System.out.println("case : 1");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[0] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[0] = "O";
 						MouseController.setSwipe(true);
 					}
 				} else if (mouseEvent.getX() > 400 && mouseEvent.getX() < 800 && mouseEvent.getY() < 266) {
-					System.out.println("case : 2");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[1] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[1] = "O";
 						MouseController.setSwipe(true);
 					}
 				} else if (mouseEvent.getX() > 800 && mouseEvent.getY() < 266) {
-					System.out.println("case : 3");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[2] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[2] = "O";
 						MouseController.setSwipe(true);
 					}
 				} else if (mouseEvent.getX() < 400 && mouseEvent.getY() > 266 && mouseEvent.getY() < 532) {
-					System.out.println("case : 4");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[3] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[3] = "O";
 						MouseController.setSwipe(true);
 					}
 				} else if (mouseEvent.getX() > 400 && mouseEvent.getX() < 800 && mouseEvent.getY() > 266 && mouseEvent.getY() < 532) {
-					System.out.println("case : 5");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[4] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[4] = "O";
 						MouseController.setSwipe(true);
 					}
 				} else if (mouseEvent.getX() > 800 && mouseEvent.getY() > 266 && mouseEvent.getY() < 532) {
-					System.out.println("case : 6");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[5] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[5] = "O";
 						MouseController.setSwipe(true);
 					}
 				} else if (mouseEvent.getX() < 400 && mouseEvent.getY() > 532) {
-					System.out.println("case : 7");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[6] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[6] = "O";
 						MouseController.setSwipe(true);
 					}
 				} else if (mouseEvent.getX() > 400 && mouseEvent.getX() < 800 && mouseEvent.getY() > 532) {
-					System.out.println("case : 8");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[7] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[7] = "O";
 						MouseController.setSwipe(true);
 					}
 				} else if (mouseEvent.getX() > 800 && mouseEvent.getY() > 532) {
-					System.out.println("case : 9");
 					if (MouseController.isSwipe()) {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[8] = "X";
 						MouseController.setSwipe(false);
 					} else {
-						System.out.println(MouseController.isSwipe());
 						tictactoe[8] = "O";
 						MouseController.setSwipe(true);
 					}
 				}
-				for(String o : tictactoe) {
+				for (String o : tictactoe) {
 					System.out.print(o);
 				}
+				checkWinner(g);
 
 			}
 		});
+	}
+
+	static void checkWinner(Graphics2D g) {
+		for (int a = 0; a < 8; a++) {
+			String line = null;
+
+			// spécial pour Nico
+			switch (a) {
+				case 0:
+					line = tictactoe[0] + tictactoe[1] + tictactoe[2];
+					break;
+				case 1:
+					line = tictactoe[3] + tictactoe[4] + tictactoe[5];
+					break;
+				case 2:
+					line = tictactoe[6] + tictactoe[7] + tictactoe[8];
+					break;
+				case 3:
+					line = tictactoe[0] + tictactoe[3] + tictactoe[6];
+					break;
+				case 4:
+					line = tictactoe[1] + tictactoe[4] + tictactoe[7];
+					break;
+				case 5:
+					line = tictactoe[2] + tictactoe[5] + tictactoe[8];
+					break;
+				case 6:
+					line = tictactoe[0] + tictactoe[4] + tictactoe[8];
+					break;
+				case 7:
+					line = tictactoe[2] + tictactoe[4] + tictactoe[6];
+					break;
+			}
+			if (line.equals("XXX")) {
+				g.drawString("Le joueur 1 à gagné", 500, 400);
+			} else if (line.equals("OOO")) {
+				g.drawString("Le joueur 2 à gagné", 500, 400);
+			}
+		}
+
 	}
 
 	public void mousePressed(MouseEvent mouseEvent) {

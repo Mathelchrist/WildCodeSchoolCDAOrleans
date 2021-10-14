04a094e4614d68bba12ba46cc1122e55c908f675
HOARAU <Mathieuhoarau@laposte.net>
2021-10-14 16:52:06 +0200

    caseClick

--- Projects/20211014_small_games/src/main/java/wcscda/small_game/MouseController.java
+++ Projects/20211014_small_games/src/main/java/wcscda/small_game/MouseController.java
@@ -12,7 +12,6 @@ public class MouseController extends MouseAdapter {
     private final Board board;
     private static boolean click = true;
     private static String[] tictactoe = new String[9];
-    private static ArrayList<Integer> savePosXY = new ArrayList<Integer>();
 
     public MouseController(Board board) {
         this.board = board;
@@ -31,58 +30,101 @@ public class MouseController extends MouseAdapter {
         });
     }
 
-    public static ArrayList<Integer> getSavePosXY() {
-        return savePosXY;
-    }
-
-    public static void setSavePosXY(ArrayList<Integer> savePosXY) {
-        MouseController.savePosXY = savePosXY;
-    }
-
     public static void clickForm(Graphics2D g, int x, int y) {
         if (click) {
             g.drawLine(x - 30, y - 30, x + 30, y + 30);
             g.drawLine(x + 30, y - 30, x - 30, y + 30);
             click = false;
             
-            savePosXY.add(x);
-            savePosXY.add(y);
         } else {
             g.drawOval(x - 50, y - 50, 100, 100);
             click = true;
-            savePosXY.add(x);
-            savePosXY.add(y);
         }
     }
 
     public void mousePressed(MouseEvent mouseEvent) {
+        
     	if(mouseEvent.getX() < 400 && mouseEvent.getY() < 266) {
-    		System.out.println("LOL1");
-    		System.out.println(click);
+			System.out.println("case : 1");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[0] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[0] = "O";
+			}
     	}else if(mouseEvent.getX() > 400 && mouseEvent.getX() < 800 && mouseEvent.getY() < 266) {
-    		System.out.println("LOL2");
-    		System.out.println(click);
+    		System.out.println("case : 2");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[1] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[1] = "O";
+			}
     	}else if(mouseEvent.getX() > 800 && mouseEvent.getY() < 266) {
-    		System.out.println("LOL3");
-    		System.out.println(click);
+    		System.out.println("case : 3");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[2] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[2] = "O";
+			}
     	}else if(mouseEvent.getX() < 400 && mouseEvent.getY() > 266 && mouseEvent.getY() < 532) {
-    		System.out.println("LOL4");
-    		System.out.println(click);
+    		System.out.println("case : 4");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[3] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[3] = "O";
+			}
     	}else if(mouseEvent.getX() > 400 && mouseEvent.getX() < 800 && mouseEvent.getY() > 266 && mouseEvent.getY() < 532) {
-    		System.out.println("LOL5");
-    		System.out.println(click);
+    		System.out.println("case : 5");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[4] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[4] = "O";
+			}
     	}else if(mouseEvent.getX() > 800 && mouseEvent.getY() > 266 && mouseEvent.getY() < 532) {
-    		System.out.println("LOL6");
-    		System.out.println(click);
+    		System.out.println("case : 6");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[5] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[5] = "O";
+			}
     	}else if(mouseEvent.getX() < 400 && mouseEvent.getY() > 532) {
-    		System.out.println("LOL7");
-    		System.out.println(click);
+    		System.out.println("case : 7");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[6] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[6] = "O";
+			}
     	}else if(mouseEvent.getX() > 400 && mouseEvent.getX() < 800 && mouseEvent.getY() > 532) {
-    		System.out.println("LOL8");
-    		System.out.println(click);
+    		System.out.println("case : 8");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[7] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[7] = "O";
+			}
     	}else if(mouseEvent.getX() > 800 && mouseEvent.getY() > 532) {
-    		System.out.println("LOL9");
-    		System.out.println(click);
+    		System.out.println("case : 9");
+			if(click) {
+				System.out.println("case : 1 true");
+				tictactoe[8] = "X";
+			}else {
+				System.out.println("case : 1 false");
+				tictactoe[8] = "O";
+			}
     	}
     }
 }
